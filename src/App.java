5b3bd60b668a8772378ec26ece0019786622f274
Dharejo-Junaid <junaidali.100190@gmail.com>
2023-05-18 18:32:33 +0100

    Nimbus look and feel add into App class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import javax.swing.UIManager;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
         new LoginPage();
     }
 }
